5895a8c70e5d8a1b580d0367e73a18ee8dda26c0
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-03-01 22:12:59 +0530

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Button } from './ui/button'
+import DarkModeToggle from './DarkModeToggel'
 
 function Header() {
   return (
     
     <div>
-      
+      <DarkModeToggle />
     </div>
   )
 }

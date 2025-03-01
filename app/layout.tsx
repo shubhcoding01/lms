d18b5b09d6962882d9a1327b7bdbbd86ef1fa390
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-03-01 22:08:29 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     
-    <html lang="en">
+    <html lang="en" 
+    suppressHydrationWarning
+    >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

02c17130af5095dc05e083e39cd4fba493349bdc
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-03-02 12:24:26 +0530

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import DarkModeToggle from './DarkModeToggel'
 function Header() {
   return (
     
-    <header className="sticky top-0 right-0 left-0 z-10 
+    <header className="sticky top-0 z-10 
        bg-background/80 backdrop-blur-sm border-b border-border">
       <DarkModeToggle />
     </header>

914c790f41a2a282664b52318a8de0bbc69183fd
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-03-03 09:24:14 +0530

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,9 @@ function Header() {
     
     <header className="sticky top-0 z-10 
        bg-background/80 backdrop-blur-sm border-b border-border">
+        <div className='container mx-auto px-4'>
       <DarkModeToggle />
+      </div>
     </header>
   )
 }

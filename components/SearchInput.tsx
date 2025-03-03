7ce329f48a7172037725335a65080ef31614fade
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-03-03 22:38:07 +0530

    Update

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -18,7 +18,7 @@ function SearchInput() {
         className='w-full rounded-full bg-secondary/80 px-4 py-2 pl-10 text-sm focus:outline-none 
         focus:ring-2 focus:ring-primary '
         />
-        <Search />
+        <Search className="absolute left-3 h-4 w-4 top-1/2 -translate-y-1/2 text-muted-foreground" />
     </form>
   )
 }

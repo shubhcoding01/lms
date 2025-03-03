c1f7eda7a914bfbd80aae0c68a75be7c1868051c
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-03-03 22:21:46 +0530

    Update

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { useRouter } from "next/navigation"
+import { useState } from "react";
 
 function SearchInput() {
     const router = useRouter();
-    
+    const [searchQuery, setSearchQuery] = useState("")
   return (
     
     <div>SearchInput</div>
